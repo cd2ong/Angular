1beaab2ad644243b5afe5d4f0360453acf64162e
Christopher Ong <C.david.ong@gmail.com>
2020-03-13 10:43:50 -0400

    module 1.8 completed

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -65,4 +65,8 @@ export class ProductListComponent implements OnInit{
     ngOnInit(): void {
         console.log('In OnInit');
     }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
+    }
 }
