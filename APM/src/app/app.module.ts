2fb7560f9cbc93f15a8bcff63dbcd57f7338d57a
Christopher Ong <C.david.ong@gmail.com>
2020-03-13 13:31:55 -0400

    module 1.11 & 1.12 completed

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -7,16 +7,35 @@ import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { starComponent } from './shared/star.component';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductDetailGuard } from './products/product-detail.guard';
 
 @NgModule({
   declarations: [
     AppComponent, 
     ProductListComponent, 
     ConvertToSpacesPipe,
-    starComponent
+    starComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule
+    BrowserModule, 
+    FormsModule, 
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path:'welcome', component:WelcomeComponent},
+      {path:'products', component:ProductListComponent},
+      {
+        path:'products/:id', 
+        canActivate: [ProductDetailGuard],
+        component:ProductDetailComponent
+      },
+      {path:'', redirectTo:'welcome', pathMatch:'full'},
+      {path:'**', redirectTo:'welcome', pathMatch:'full'}
+    ], {useHash: true})
   ],
   bootstrap: [AppComponent]
 })
