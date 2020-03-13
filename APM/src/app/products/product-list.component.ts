c57e9c9e063e3f347aec77ccf4c0b7284de4a4d4
Christopher Ong <C.david.ong@gmail.com>
2020-03-13 11:54:08 -0400

    module 1.9 completed

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit} from '@angular/core'
 import { IProduct } from './product'
+import { ProductService } from './product.service'
 
 @Component({
     selector:'pm-products',
-    templateUrl: './product-list.component.html',
+    templateUrl: './product-list.component.html', 
     styleUrls: ['./product-list.component.css']
 })
 
@@ -23,33 +24,10 @@ export class ProductListComponent implements OnInit{
         this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
     }
 
+    products: IProduct[];
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2019",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-          },
-          {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2019",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-          }
-    ];
-
-    constructor(){
-        this.filteredProducts = this.products;
-        this.listFilter = "cart";
+    
+    constructor(private productService: ProductService){
     }
 
     performFilter(filterBy: string){
@@ -63,9 +41,10 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-        console.log('In OnInit');
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
     }
-
+ 
     onRatingClicked(message: string): void {
         this.pageTitle = 'Product List: ' + message;
     }
